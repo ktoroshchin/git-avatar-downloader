04c544a5cf489e9e4df738b86cf712395896addf
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-10-01 20:45:32 +0000

    Avatar downloading

--- download_avatars.js
+++ download_avatars.js
@@ -2,26 +2,42 @@ var request = require('request');
 
 var token = require('./secrets');
 
-console.log('WELCOME!!!...........');
+var fs = require('fs');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-  headers: {
-    'User-Agent': 'request',
-    'Authorization': token.GITHUB_TOKEN
-  }
-};
+// console.log('WELCOME!!!...........');
+//
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   var options = {
+//   url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//   headers: {
+//     'User-Agent': 'request',
+//     'Authorization': token.GITHUB_TOKEN
+//   }
+// };
+//
+//   request(options, function(err, res, body) {
+//     cb(err, body);
+//   });
+// }
+//
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors---->", err);
+//   var parse = JSON.parse(result)
+//   for(var contributor of parse){
+//     console.log("Result---->", contributor.avatar_url);
+//   }
+// });
 
-  request(options, function(err, res, body) {
-    cb(err, body);
-  });
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function(){
+      console.log('Downloading picture...');
+      console.log('Download complete!');
+    });
 }
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  var parse = JSON.parse(result)
-  for(var contributor of parse){
-    console.log("Result---->", contributor.avatar_url);
-  }
-});
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./downloadpicture/kvirani.jpg")
