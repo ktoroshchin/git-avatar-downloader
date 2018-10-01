0bc688d0f87beae71d7bb4f14c486ac3a03bdddc
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-10-01 21:00:15 +0000

    Downloading url pictures into folder with 'login'.jpg filenames from github

--- download_avatars.js
+++ download_avatars.js
@@ -4,40 +4,40 @@ var token = require('./secrets');
 
 var fs = require('fs');
 
-// console.log('WELCOME!!!...........');
-//
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var options = {
-//   url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//   headers: {
-//     'User-Agent': 'request',
-//     'Authorization': token.GITHUB_TOKEN
-//   }
-// };
-//
-//   request(options, function(err, res, body) {
-//     cb(err, body);
-//   });
-// }
-//
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors---->", err);
-//   var parse = JSON.parse(result)
-//   for(var contributor of parse){
-//     console.log("Result---->", contributor.avatar_url);
-//   }
-// });
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    'User-Agent': 'request',
+    'Authorization': 'token ' + token.GITHUB_TOKEN
+  }
+};
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors---->", err);
+  var parse = JSON.parse(result)
+  // console.log(parse);
+  for(var contributor of parse){
+    // var downloadUrl = contributor.avatar_url
+    // console.log("Result---->", contributor.avatar_url);
+    downloadImageByURL(contributor.avatar_url, `./downloadpicture/${contributor.login}.jpg`)
+  }
+});
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', function (err) {
       throw err;
     })
-
     .pipe(fs.createWriteStream(filePath))
     .on('finish', function(){
       console.log('Downloading picture...');
       console.log('Download complete!');
     });
 }
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./downloadpicture/kvirani.jpg")
